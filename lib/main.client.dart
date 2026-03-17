8e1dc80fc6d580c9d82d4a379c0b58686be7be1a
redmediasolutions <redmediasolutions8@gmail.com>
2026-03-17 14:38:38 +0530

    firebase setup

--- lib/main.client.dart
+++ lib/main.client.dart
@@ -1,10 +1,15 @@
-import 'package:jaspr/client.dart'; // Add this to recognize 'Jaspr'
+import 'package:jaspr/client.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'app.dart';
 import 'main.client.options.dart';
+import 'package:medzsite/util/firebase_options.dart'; 
 
-void main() {
-  // Now 'Jaspr' will be recognized correctly
+void main() async {
+  // Ensure the initialization is awaited
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   Jaspr.initializeApp(options: defaultClientOptions);
-
   runApp(App());
 }
